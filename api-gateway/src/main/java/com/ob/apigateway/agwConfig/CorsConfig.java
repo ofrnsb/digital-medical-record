b9ceaddf7b2b1ae7104867061b160796d64f0028
ofrnsb <ondifransbutarbutar@upi.edu>
2024-07-15 04:11:01 +0700

    update ip public to host

--- api-gateway/src/main/java/com/ob/apigateway/agwConfig/CorsConfig.java
+++ api-gateway/src/main/java/com/ob/apigateway/agwConfig/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     CorsConfiguration corsConfiguration = new CorsConfiguration();
     corsConfiguration.addAllowedOrigin("https://frontend-6pemi.ondigitalocean.app");
     corsConfiguration.addAllowedOrigin("http://127.0.0.1:3000");
-    corsConfiguration.addAllowedOrigin("http://134.209.96.201:3000");
+    corsConfiguration.addAllowedOrigin("https://oftech.me");
     corsConfiguration.addAllowedMethod("*");
     corsConfiguration.addAllowedHeader("*");
     corsConfiguration.setAllowCredentials(true);
